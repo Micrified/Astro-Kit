702af644542a2f57ba5a4e3dfa8fea115774101c
Owatch <Owatch@Katherine-Randolphs-MacBook-Pro.local>
2015-05-25 11:36:04 -0400

    Initial Commit

---
+++ AstroKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AstroKit
+//
+//  Created by Owatch on 5/25/15.
+//  Copyright (c) 2015 Unisung Softworks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
